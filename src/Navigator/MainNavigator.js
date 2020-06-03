61fd3d334b78a8ca7864c2ea74bf2674b0a6f8a5
dheeyi <dheeyi@gmail.com>
2020-06-03 16:56:25 -0400

    adds drawer navigation

--- src/Navigator/MainNavigator.js
+++ src/Navigator/MainNavigator.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import LoginScreen from '../View/login/LoginScreen';
 import RegisterScreen from '../View/register/Register';
@@ -10,53 +11,72 @@ import SettingScreen from '../View/Setting/SettingScreen';
 import Colors from '../Config/Colors';
 
 const Stack = createStackNavigator();
+const StackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{
+          title: 'Register Form',
+          headerStyle: {
+            backgroundColor: Colors.appPrimary,
+          },
+          headerTintColor: Colors.white,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: Colors.white,
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
 
-function mainNavigator() {
+const Drawer = createDrawerNavigator();
+const DrawerScreen = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name='App'
+        component={AppScreen}
+        options={{
+          title: 'App Main',
+          headerStyle: {
+            backgroundColor: Colors.appPrimary,
+          },
+          headerTintColor: Colors.white,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: Colors.white,
+            fontSize: 25
+          },
+        }}
+      />
+      <Drawer.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={{title: 'Settings'}}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+
+const mainNavigator = () => {
+  const [isLogged, setIsLogged] = React.useState(null);
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name='App'
-          component={AppScreen}
-          options={{
-            title: 'App Main',
-            headerStyle: {
-              backgroundColor: Colors.appPrimary,
-            },
-            headerTintColor: Colors.white,
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              color: Colors.white,
-              fontSize: 25
-            },
-          }}
-        />
-        <Stack.Screen
-          name="Setting"
-          component={SettingScreen}
-          options={{title: 'Settings'}}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{
-            title: 'Register Form',
-            headerStyle: {
-              backgroundColor: Colors.appPrimary,
-            },
-            headerTintColor: Colors.white,
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              color: Colors.white,
-            },
-          }}
-        />
-      </Stack.Navigator>
+      {isLogged !== null ? (
+        <StackScreen />
+      ) : (
+        <DrawerScreen />
+      )}
     </NavigationContainer>
   );
 }
